f518d51ced9c46936ad406c296e512fc0ac848e8
Levent Ali <lebreeze@gmail.com>
2012-03-12 07:42:53 +0000

    Redirect to cc.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :ensure_domain
+
+  TheDomain = 'curatorscoffee.com'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != TheDomain && Rails.env == "production"
+      redirect_to "http://#{TheDomain}"
+    end
+  end
 end
