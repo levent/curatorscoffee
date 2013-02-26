ac8595d726b3a307e8f02ce3bfde57a9c7b6b914
Levent Ali <lebreeze@gmail.com>
2013-02-26 07:49:11 +0000

    [heroku] config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module CuratorscoffeeCom
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
     config.assets.precompile += %w( leaflet.ie.css footer.js )
 
     config.cache_store = :dalli_store
