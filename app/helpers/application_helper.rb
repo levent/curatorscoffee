a4b294529ebb400c3eb4734bd1d3621df793d423
Levent Ali <lebreeze@gmail.com>
2013-06-24 11:05:40 +0100

    strike expired classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def class_time(timestamp)
+    time = timestamp.to_time
+    time_string = time.strftime('%A %e %B, %l:%M%P')
+    if time < 1.hours.ago
+      content_tag :strike do
+        time_string
+      end
+    else
+      time_string
+    end
+  end
 end
