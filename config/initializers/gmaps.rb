83c787766961e2c503e8387496a94f15d3f955d8
Levent Ali <lebreeze@gmail.com>
2013-07-10 21:02:02 +0100

    GMAPS api key staging

--- config/initializers/gmaps.rb
+++ config/initializers/gmaps.rb
@@ -1,4 +1,4 @@
-if Rails.env == 'development'
+if Rails.env == 'development' || ENV['STAGING'] == 'yes'
   GOOGLE_API_KEY = "AIzaSyBmMwTQNURf5Alm1F_qk4IQAv5DE9q2NZw"
 else
   GOOGLE_API_KEY = "AIzaSyAa_Co2CAy9HFbCgA3A8teQabO8K8EeCPg"
