9e04cc5f3a890d73cba9b20413b4b7af1c66542c
Levent Ali <lebreeze@gmail.com>
2013-07-10 21:22:30 +0100

    New key

--- config/initializers/gmaps.rb
+++ config/initializers/gmaps.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'development' || ENV['STAGING'] == 'yes'
-  GOOGLE_API_KEY = "AIzaSyBmMwTQNURf5Alm1F_qk4IQAv5DE9q2NZw"
+  GOOGLE_API_KEY = "AIzaSyCgLmq2efF5ZVkptbgnxm15OI70zZFzhyE"
 else
   GOOGLE_API_KEY = "AIzaSyAa_Co2CAy9HFbCgA3A8teQabO8K8EeCPg"
 end
