45174f3764d4fcc0972ff20ef775165a85c2c38c
Levent Ali <lebreeze@gmail.com>
2013-07-10 07:36:24 +0100

    [staging] cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,11 @@ CuratorscoffeeCom::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "//dh2gjjtg5ju5o.cloudfront.net"
+  if ENV['STAGING'] == 'yes'
+    config.action_controller.asset_host = "//d1l4y0v712sto9.cloudfront.net"
+  else
+    config.action_controller.asset_host = "//dh2gjjtg5ju5o.cloudfront.net"
+  end
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
