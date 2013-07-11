0ca1c9e9859196bf7efc1d9d7db4b2a29c167e0c
Levent Ali <lebreeze@gmail.com>
2013-07-11 07:15:18 +0100

    [gmaps] key shuffle

--- config/initializers/gmaps.rb
+++ config/initializers/gmaps.rb
@@ -1,4 +1,4 @@
-if Rails.env == 'development' || ENV['STAGING'] == 'yes'
+if Rails.env == 'development'
   GOOGLE_API_KEY = "AIzaSyCgLmq2efF5ZVkptbgnxm15OI70zZFzhyE"
 else
   GOOGLE_API_KEY = "AIzaSyAa_Co2CAy9HFbCgA3A8teQabO8K8EeCPg"
