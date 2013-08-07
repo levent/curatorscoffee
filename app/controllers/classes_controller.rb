2a98f73d3a645a3daf271a404c7fcc6998f2d319
Levent Ali <lebreeze@gmail.com>
2013-08-07 07:13:50 +0100

    Cache with ttl

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -4,11 +4,11 @@ class ClassesController < ApplicationController
     @latte_classes = Rails.cache.read('latte_classes')
     unless @brew_classes
       @brew_classes = BrewClass.by_scheduled_at.startkey(1.week.ago).rows
-      Rails.cache.write('brew_classes', @brew_classes)
+      Rails.cache.write('brew_classes', @brew_classes, :expires_in => 3600)
     end
     unless @latte_classes
       @latte_classes = LatteArtClass.by_scheduled_at.startkey(1.week.ago).rows
-      Rails.cache.write('latte_classes', @latte_classes)
+      Rails.cache.write('latte_classes', @latte_classes, :expires_in => 3600)
     end
   end
 end
