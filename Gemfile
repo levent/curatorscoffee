e1d7f95cf96207c266b42db2faf54393b6befa27
Levent Ali <lebreeze@gmail.com>
2014-07-12 17:39:06 +0100

    Fix js bugs

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ ruby "2.1.2"
 gem 'rails', '4.1.4'
 gem 'sass-rails', '~> 4.0.3'
 gem 'coffee-rails', '~> 4.0.0'
-gem 'jquery-rails'
 gem 'turbolinks'
 gem 'spring',        group: :development
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,4 @@
 //= require jquery-2.1.1.min
-//= require jquery_ujs
 //= require foundation/foundation
 //= require foundation/foundation.orbit
-//= require turbolinks
-//= require curatorscoffee
+$(document).foundation();
