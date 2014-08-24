1241f7c23f98aeed5095d80755951a629d926e8e
Levent Ali <lebreeze@gmail.com>
2014-08-24 09:15:07 +0100

    Cache for 10 minutes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   before_filter :set_map_urls
-  caches_action :index
-  caches_action :locations
-  caches_action :design
-  caches_action :creations
-  caches_action :food_and_drink
+  caches_action :index, expires_in: 10.minutes
+  caches_action :locations, expires_in: 10.minutes
+  caches_action :design, expires_in: 10.minutes
+  caches_action :creations, expires_in: 10.minutes
+  caches_action :food_and_drink, expires_in: 10.minutes
 
   def index
   end
