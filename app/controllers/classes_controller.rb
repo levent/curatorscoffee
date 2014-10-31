2a85ba7d359710961922fa09cd19518d3aae4ea7
Levent Ali <lebreeze@gmail.com>
2014-10-31 07:27:44 +0000

    Remove couchdb from classes page

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,19 +1,4 @@
 class ClassesController < ApplicationController
   def index
-    @brew_classes = Rails.cache.read('brew_classes')
-    @latte_classes = Rails.cache.read('latte_classes')
-    @tea_classes = Rails.cache.read('tea_classes')
-    unless @brew_classes
-      @brew_classes = CouchPotato.database.view BrewClass.all(:key => (1.week.ago)..(1.year.from_now))
-      Rails.cache.write('brew_classes', @brew_classes, :expires_in => 3600)
-    end
-    unless @latte_classes
-      @latte_classes = CouchPotato.database.view LatteArtClass.all(:key => (1.week.ago)..(1.year.from_now))
-      Rails.cache.write('latte_classes', @latte_classes, :expires_in => 3600)
-    end
-    unless @tea_classes
-      @tea_classes = CouchPotato.database.view TeaClass.all(:key => (1.week.ago)..(1.year.from_now))
-      Rails.cache.write('tea_classes', @tea_classes, :expires_in => 3600)
-    end
   end
 end
