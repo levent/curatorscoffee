e269c81a27ce31d560a0747dec444745168d2766
Levent Ali <lebreeze@gmail.com>
2014-11-29 12:43:18 +0000

    [epic] [admin] Remove all the admin stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,2 @@
 module ApplicationHelper
-
-  def format_date(timestamp)
-    return nil unless timestamp
-    timestamp.to_date.to_formatted_s(:long_ordinal)
-  rescue
-    return nil
-  end
-
-  def class_time(klass)
-    time_string = klass.scheduled_at.strftime('%A %e %B, %l:%M%P')
-    if klass.scheduled_at < 1.hours.ago || klass.available <= 0
-      content_tag :strike do
-        time_string
-      end
-    else
-      time_string
-    end
-  end
 end
--- app/models/latte_art_class.rb
+++
@@ -1,3 +0,0 @@
-class LatteArtClass < CoffeeClass
-  property :available, :type => Fixnum, :default => 4
-end
